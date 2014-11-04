5b33a55575e96cbf9f0cd6d228f81a375a92d48f
kradnozd <kradnozd@gmail.com>
2014-11-04 23:44:02 +0700

    0.3.6

--- app/controllers.js
+++ app/controllers.js
@@ -9,11 +9,11 @@ dota2tvControllers.controller('HomepageCtrl', ['$scope', '$http',
     $scope.orderProp = 'age';
   }]);
 
-dota2tvControllers.controller('WatchpageCtrl', ['$scope', '$http', '$routeParams',
-  function ($scope, $http, $routeParams) {
+dota2tvControllers.controller('WatchpageCtrl', ['$scope', '$http', '$routeParams', '$sce', 
+  function ($scope, $http, $routeParams, $sce) {
   	switch($routeParams.streamService) {
   		case "tw":
-  			$scope.streamVid = '<iframe src="http://www.twitch.tv/' + $routeParams.streamID + '/embed" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>';
+  			$scope.streamVid = $sce.trustAsHtml('<iframe src="http://www.twitch.tv/' + $routeParams.streamID + '/embed" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>');
   			break;
   	};
   }]);
