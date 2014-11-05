22cf458ae6a3e1f5875b10e4215c766be255a3e9
kradnozd <kradnozd@gmail.com>
2014-11-05 13:58:08 +0700

    0.3.8

--- app/controllers.js
+++ app/controllers.js
@@ -12,9 +12,25 @@ dota2tvControllers.controller('HomepageCtrl', ['$scope', '$http',
 dota2tvControllers.controller('WatchpageCtrl', ['$scope', '$http', '$routeParams', '$sce', 
   function ($scope, $http, $routeParams, $sce) {
   	switch($routeParams.streamService) {
+  		// Twitch.tv
   		case "tw":
   			$scope.streamVid = $sce.trustAsHtml('<iframe src="http://www.twitch.tv/' + $routeParams.streamID + '/embed" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>');
   			$scope.streamChat = $sce.trustAsHtml('<iframe src="http://www.twitch.tv/' + $routeParams.streamID + '/chat?popout=" frameborder="0" scrolling="no" height="100%" width="280"></iframe>');
+  			break;
+  		// Csmtalk
+  		case "cc":
+  			$scope.streamVid = $sce.trustAsHtml('<iframe scrolling="no" src="http://talktv.vn/streaming/play/embed/' + $routeParams.streamID + '" height="100%" width="100%"></iframe>');
+  			$scope.streamChat = $sce.trustAsHtml('<iframe scrolling="no" src="http://talktv.vn/streaming/chat/embed/' + $routeParams.streamID + '" height="100%" width="280"></iframe>');
+  			break;
+  		// Dailymotion
+  		case "dm":
+  			$scope.streamVid = $sce.trustAsHtml('<iframe frameborder="0" width="100%" height="100%" src="//www.dailymotion.com/embed/video/' + $routeParams.streamID + '" allowfullscreen></iframe>');
+  			$scope.streamChat = $sce.trustAsHtml('<iframe frameborder="0" scrolling="no" id="live_chat" src="http://www.dailymotion.com/livechat/' + $routeParams.streamID + '" style="background:#fff;" height="100%" width="100%" allowfullscreen=""></iframe>');
+  			break;
+  		// Hitbox
+  		case "hb":
+  			$scope.streamVid = $sce.trustAsHtml('<iframe width="100%" height="100%" src="http://hitbox.tv/#!/embed/' + $routeParams.streamID + '" frameborder="0" allowfullscreen></iframe>');
+  			$scope.streamChat = $sce.trustAsHtml('<iframe width="100%" height="100%" src="http://www.hitbox.tv/embedchat/' + $routeParams.streamID + '" frameborder="0" allowfullscreen></iframe>');
   			break;
   	};
   }]);
