ea604b8a0aea8650b7d22135576b62a2be7a0bfd
kradnozd <Krad@Tungs-Mac-Pro.local>
2014-11-10 02:24:00 +0700

    0.9.7

--- app/app.js
+++ app/app.js
@@ -19,20 +19,9 @@ dota2tv.config(['$routeProvider',
       });
   }]);
 
-dota2tv.directive('channelsList', ['', function(){
-  // Runs during compile
+dota2tv.directive('channelsList', function() {
   return {
-    // name: '',
-    // priority: 1,
-    // terminal: true,
-    // scope: {}, // {} = isolate, true = child, false/undefined = no change
-    // controller: function($scope, $element, $attrs, $transclude) {},
-    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-    template: 'FUCK YOU BABY!',
-    // templateUrl: '',
-    replace: true
-    // transclude: true,
-    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+    restrict    : 'E',  // used E because of element 
+    templateUrl : 'app/views/channelsList.html'
   };
-}]);
+});
