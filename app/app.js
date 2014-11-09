11339b75651f60b231b39880d10f9f1e136ed8d0
kradnozd <Krad@Tungs-Mac-Pro.local>
2014-11-10 02:04:58 +0700

    0.9.5

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 var dota2tv = angular.module('dota2tv', [
 'ngRoute',
-'dota2tvControllers',
-'dota2tvDirectives'
+'dota2tvControllers'
 ]);
 
 dota2tv.config(['$routeProvider',
@@ -19,3 +18,24 @@ dota2tv.config(['$routeProvider',
         redirectTo: '/404'
       });
   }]);
+
+dota2tv.directive('channelsList', ['', function(){
+  // Runs during compile
+  return {
+    // name: '',
+    // priority: 1,
+    // terminal: true,
+    // scope: {}, // {} = isolate, true = child, false/undefined = no change
+    // controller: function($scope, $element, $attrs, $transclude) {},
+    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+    template: 'FUCK YOU BABY!',
+    // templateUrl: '',
+    // replace: true,
+    // transclude: true,
+    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+    link: function($scope, iElm, iAttrs, controller) {
+      
+    }
+  };
+}]);
--- app/directives.js
+++ app/directives.js
@@ -1,10 +1,1 @@
 var dota2tvDirectives = angular.module('dota2tvDirectives', []);
-
-dota2tvDirectives.directive('channelsList', 
-function () {
-  return {
-    restrict: 'E',
-    replace: 'true',
-    template: 'Hello boy!'
-  };
-});
